2480e494fb99d7db07acd0d688c15955e632939b
ricardodalarme <ricardo.dalarme@profusion.mobi>
2024-06-24 13:44:31 -0300

    refactor: make rule type an enum

--- lib/src/analyzers/lint_analyzer/rules/models/rule_type.dart
+++ lib/src/analyzers/lint_analyzer/rules/models/rule_type.dart
@@ -1,12 +1,12 @@
 /// Represents a rule type. Used by documentation path construction.
-class RuleType {
+enum RuleType {
+  common('common'),
+  flutter('flutter'),
+  intl('intl'),
+  angular('angular'),
+  flame('flame');
+
   final String value;
 
-  const RuleType._(this.value);
-
-  static const common = RuleType._('common');
-  static const flutter = RuleType._('flutter');
-  static const intl = RuleType._('intl');
-  static const angular = RuleType._('angular');
-  static const flame = RuleType._('flame');
+  const RuleType(this.value);
 }
